1c7f3cab4ea6d38987f06da6baed37d6cedbc9f2
ktgcreative <ktgcreative@gmail.com>
2023-11-16 00:54:03 +1300

    home-refactor

--- components/HomePage/page.tsx
+++ components/HomePage/page.tsx
@@ -2,42 +2,40 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function HomePage() {
-  const router = useRouter();
+const heroContent = {
+  title: "Kyvingus University",
+  subtitle1: "Building a Centre of excellence",
+  subtitle2: "in Legal Education",
+  description: "Programmes Approved by Bar Council of Your Country",
+  buttonText: "Enroll Now"
+};
 
+export default function HomePage() {
+  
+  const router = useRouter();
   const handleEnrollNowClick = () => {
     router.push("/login");
   };
+
   return (
-    <div style={{ position: "relative" }}>
+    <div className="relative">
       <img className="image" src="/background.png" alt="" />
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "start",
-          gap: "8px",
-          padding: "200px",
-          background: "rgba(0, 0, 0, 0)",
-          color: "white",
-        }}
-      >
-        <div className="text-[50px] font-medium font-Poppins leading-[75px]">
-          Kyvingus University <br />
-          Building a Centre of excellence <br />
-          in Legal Education
-        </div>
-        <div className="text-[23px] font-light font-Poppins leading-[34.50px]">
-          Programmes Approved by Bar Council of Your Country
-        </div>
+      <div className="absolute mt-14 top-0 left-0 flex flex-col items-start gap-2 p-8 md:p-20 lg:p-50 text-white">
+        <h1 className="text-5xl md:text-7xl lg:text-[60px] font-bold leading-tight md:leading-snug lg:leading-[95px]">
+          {heroContent.title}
+        </h1>
+        <h2 className="text-3xl md:text-5xl lg:text-[50px] font-medium leading-tight md:leading-snug lg:leading-[55px]">
+          {heroContent.subtitle1} <br />
+          {heroContent.subtitle2}
+        </h2>
+        <p className="text-lg md:text-xl lg:text-[23px] font-light leading-snug md:leading-relaxed lg:leading-[34.5px]">
+          {heroContent.description}
+        </p>
         <button
-          className="bg-red-500 px-4 py-2 text-2xl font-normal font-Poppins leading-9 text-white"
+          className="bg-red-500 px-4 py-2 text-lg md:text-xl lg:text-[2xl] font-normal rounded-xl leading-9 text-white"
           onClick={handleEnrollNowClick}
         >
-          Enroll Now
+          {heroContent.buttonText}
         </button>
       </div>
     </div>
